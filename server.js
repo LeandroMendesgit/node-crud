24c01c5ccd1887e94295a6e0633ed3642f2c4e96
LeandroMendesgit <124472549+LeandroMendesgit@users.noreply.github.com>
2023-08-24 09:47:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 
-const app =("express");
 
-varcorsOptions= {
+const app = express();
+
+var corsOptions = {
     origin: "http://localhost:8881"
 }
 
@@ -11,13 +12,20 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended:true}));
-
-app.get("/",(req,res) => {
-     res.json({message: 'Hello World!'})
+app.use(express.urlencoded({extended: true}));
+db.sequelize.sync()
+.then(() => {
+    console.log("Banco conectado!");
+})
+.catch((err) => {
+    console.log("Falha ao acessar banco de dados: "+err.message);
+})
+app.get("/", (req,res) => {
+    res.json({message: "Hello World!"})
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT,() => {
-    console.log(`Server funcionando na porta ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server funcionado na porta ${PORT}.`)
+    ;
 })
