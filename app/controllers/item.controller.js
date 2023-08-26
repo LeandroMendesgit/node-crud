3b5c9af6136fd341d0768e443bfd368049007f65
LeandroMendesgit <leandromendes97@gmail.com>
2023-08-26 11:11:20 -0300

    Adicionando Arquivo de criação de Itens

--- app/controllers/item.controller.js
+++ app/controllers/item.controller.js
@@ -3,12 +3,48 @@ const Items = db.item;
 const Op = db.Sequelize.Op;
 
 exports.create =(req, res) => {
-    
-};
+  if (req.body.name) {
+      res.status(400).send({
+          message: "O conteúdo não pode ser vazio!"
+      });
+      return;
+      }
 
-exports.findAll = (req ,res) => {
-
+      const item = {
+          name: req.body.name,
+          description: req.body.description,
+          quantity: req.body.quantity,
+          is_flammable: req.body.is_flammable ? req.body.is_flammable : false
+     }
 };
+Item.create(item)
+.then(data => {
+    res.send(data);
+})
+.catch(err => {
+    res.status(500).send({
+        message:
+        err.message || "Ocorreu um erro o criar o item."
+})
+})
+
+
+exports.findAll = (req ,res) => { 
+    const name = req.body.name;
+    var condition = name ? {name:{ [Op.like]:  `%${name}%` } } : null;    
+
+
+   Items.findAll({where: condition})
+   .then(data => {
+       res.send(data);
+   })
+   .catch(err => {
+       res.status(500).send({
+           message:
+           err.message || "Ocorreu um erro ao listar os itens."
+       })
+    })
+    };
 
 exports.findOne=(req, res) => {
 
