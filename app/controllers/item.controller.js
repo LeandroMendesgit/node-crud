ff9506635e42cf25714afb1900a19726d054cfc7
LeandroMendesgit <leandromendes97@gmail.com>
2023-08-29 11:04:52 -0300

    Adicionando Arquivos de criação  ao servidor

--- app/controllers/item.controller.js
+++ app/controllers/item.controller.js
@@ -24,7 +24,7 @@ Item.create(item)
 .catch(err => {
     res.status(500).send({
         message:
-        err.message || "Ocorreu um erro o criar o item."
+        err.message || "Ocorreu um erro ao criar o item."
 })
 })
 
@@ -45,18 +45,73 @@ exports.findAll = (req ,res) => {
        })
     })
     };
-
 exports.findOne=(req, res) => {
+const id = req.params.id;
 
-};
+Item.findByPk(id)
+   .then(data => {
+   if (data) {
+res.send(data);
+} else {
+    res.status(404).send({
+    message: `Não foi possível encontrar um item com o id=${id}.`    
+});
+}
+})
+.catch(err => {
+    res.status(500).send({
+    message: "Ocorreu um erro ao tentar encontrar um item com o id=" + id
+    });
+    });
+    };
 
 exports.update = (req, res) =>{
+    const id = req.params.id;
 
-};
+    Item.update(req.body, {
+        where: { id: id }
+    })
+     .then(num => { 
+       if (num ==1) {
+           res.send({
+        message: "O item foi atualizado de maneira bem sucedida."
+           });    
+       } else {
+           res.send({
+             message: `Não foi possível atualizar o item com o id=${id}.`
+           });       
+           }       
+           })
+           .catch(err => {
+               res.status(500).send({
+               message: "Ocorreu um erro o tentar atualizar um item com o id=" + id
+               });
+            });
+           };
 
 exports.delete =(req, res) => {
+       const id = red.params.id;
 
-};
+       Item.destroy({
+           where: { id : id } 
+       })
+      .then(num => {
+       if(num ==1) {
+           res.send({
+               message: "O item foi apagado com sucesso"
+           });
+       } else {
+         res.send({
+             message: `Não foi possível apagar o item com o id=${id}.`
+         });
+        }
+       })
+       .catch(err => {
+           res.status(500).send({
+               message: "Ocorreu um erro ao tentar apagar o item com o id=" + id
+           });
+        });   
+    };
 
 exports.deleteAll =(req, res) => {
 
